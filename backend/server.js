eb3c945b443bd9b004d97af91195e9d22155606f
dhruvpatil1215 <148572263+dhruvpatil1215@users.noreply.github.com>
2026-06-26 09:47:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(
 );
 
 // Handle preflight requests for all routes
-app.options("*", cors());
+app.options("/*", cors());
 
 app.use(express.json());
 
@@ -66,4 +66,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
-});
+});
